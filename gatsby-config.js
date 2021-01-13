33fde2f675379e9e6810aef622d826def103e1c4
mraquinn <73719555+mraquinn@users.noreply.github.com>
2021-01-13 14:53:42 +0000

    Alex breaking everything

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,15 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-22828222-1", // Google Analytics / GA
+        ]
+      },
+    },
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,13 +13,12 @@ const IndexPage = () => (
       </div>
 
       <div className={styles.contentContainer}>
-        <h1>Takeaway pizza, beer and coffee available every Friday night!</h1>
+        <h1>Takeaway pizza & tapas available every Friday night!</h1>
         <h3>Pre-orders and collection only between 5pm and 9pm.</h3>
         <h3>Last orders 12 noon Friday.</h3>
         
-        Please call <a href="tel:01789204712">01789204712</a> to order.
+        <h3>Please check out the menu <a href={Menu} target="_blank" rel="noreferrer" className={styles.menuButton}>here</a> and call <a href="tel:01789204712">01789204712</a> to order.</h3>
 
-        Please see the <a href={Menu} target="_blank">Menu</a> here
       </div>
     </div>
   </Layout>
